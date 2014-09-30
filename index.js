686e7f5fe9c7325ac37772786948b188721fd355
Brady Garvin <brady.j.garvin@gmail.com>
2014-09-29 20:33:40 -0500

    Added option to allow console use.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
+/*global console*/ // This line tells jsLint that you're allowed to use console.log for printing.
 
